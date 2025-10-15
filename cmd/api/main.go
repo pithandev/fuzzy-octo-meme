f68a17bd904b903160c62eed2b9feb9dd8bf8337
pithandev <pithanbala7@gmail.com>
2025-10-15 12:33:04 -0300

    working on a simple http request for test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,9 @@
-package api
+package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/pithandev/fuzzy-octo-meme/internal/database"
 )
@@ -10,7 +12,7 @@ func main() {
 	// 1) connecto to db
 	// 2) check if connection is successfully
 	if err := database.Connect(); err != nil {
-		fmt.Print("Error trying to connect with database!")
+		log.Fatal("❌ Database connection failed:", err)
 	}
 
 	// 3) print successfull message
@@ -18,5 +20,16 @@ func main() {
 	fmt.Print("DB CONNECTED!")
 
 	// 4) keep program running
+
+	port := ":8080"
+	fmt.Printf("🚀 Server starting on port http://localhost%s\n", port)
+
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("✅ API is healthy"))
+	})
+
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal("❌ Server failed:", err)
+	}
 
 }
